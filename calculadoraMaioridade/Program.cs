87221b7cfe0762a5c2c50c2bc503db2ab81ade21
AnnyaB <anabordin2008@gmail.com>
2024-04-18 20:41:53 -0300

    Adicionar arquivos de projeto.

---
+++ calculadoraMaioridade/Program.cs
@@ -0,0 +1,24 @@
+﻿// faça um programa que leia o ano que a pessoa nasceu e informe se ela é maior de idade;
+int ano;
+int idade;
+int anoAtual = 2024;
+
+Console.WriteLine("Que ano você nasceu?");
+ano = int.Parse(Console.ReadLine());
+
+if (ano > anoAtual)
+{
+    Console.WriteLine("sério? Veio do futuro?");
+}
+else
+{
+    idade = anoAtual - ano;
+    if (idade >= 18)
+    {
+        Console.WriteLine("Você é maior de idade");
+    }
+    else
+    {
+        Console.WriteLine("Você é menor de idade");
+    }
+}
